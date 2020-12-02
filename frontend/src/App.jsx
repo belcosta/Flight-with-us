7764b5cf0aae9e7d5982cc151a14b9406d0b4cb9
Alice-Rez <reznickova.alice@seznam.cz>
2020-12-02 15:41:29 +0100

    added context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,46 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import "./App.css";
+import appContext from "./context";
 
 function App() {
+  const [search, setSearch] = useState({});
+  const [filter, setFilter] = useState({});
+  const [results, setResults] = useState([]);
+
+  const [cities, getCities] = useState({});
+
+  // useEffect(() => {
+  //   Axios({
+  //     method: "GET",
+  //     url: "http://localhost:3500/flights/get/cities",
+  //   })
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  const getValueSearch = (e) => {
+    setSearch((prevSearch) => {
+      return { ...prevSearch, [e.target.name]: e.target.value };
+    });
+  };
+
+  const getValueFilter = (e) => {
+    setFilter((prevFilter) => {
+      return { ...prevFilter, [e.target.name]: e.target.value };
+    });
+  };
+
+  const getResults = () => {};
+
   return (
-    <div>
-      <p>test</p>
-    </div>
+    <appContext.Provider
+      value={{ search, setSearch, filter, setFilter, results, setResults }}
+    >
+      <div>
+        <p>test</p>
+      </div>
+    </appContext.Provider>
   );
 }
 
