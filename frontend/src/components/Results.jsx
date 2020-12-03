7359b0efc910c0dc660272ebb2d8eec7273a65ec
belccarvalho <belccarvalho@gmail.com>
2020-12-03 16:18:27 +0100

    small correction on flight cards

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,6 +1,6 @@
 // import React, {useContext} from "react";
 import React from "react";
-import {Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import FlightCard from "./FlightCard";
 // import appContext from "../context";
 
@@ -18,8 +18,11 @@ function Results() {
         className="card-group bg-light mx-auto px-5 pt-5 border border-light rounded-right-top rounded-left-bottom shadow-sm"
         style={{ maxWidth: "50rem" }}
       >
-        {results.map((flight)=><FlightCard />)}
-        
+        {results.map((flight, index) => (
+          <div key={index}>
+            <FlightCard />
+          </div>
+        ))}
       </Container>
 
       <h2 className="text-center my-5">Direction Back</h2>
@@ -27,7 +30,11 @@ function Results() {
         className="card-group bg-light mx-auto px-5 pt-5 border border-light rounded-right-top rounded-left-bottom shadow-sm"
         style={{ maxWidth: "50rem" }}
       >
-        {results.map((flight)=><FlightCard />)}
+        {results.map((flight, index) => (
+          <div key={index}>
+            <FlightCard />
+          </div>
+        ))}
       </Container>
     </React.Fragment>
     // </appContext.Provider>
