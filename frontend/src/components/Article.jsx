842c942aa2dfd728a25279ca6470570e788a3ced
Alice-Rez <reznickova.alice@seznam.cz>
2020-12-20 00:31:09 +0100

    added article infos

--- frontend/src/components/Article.jsx
+++ frontend/src/components/Article.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import "./Article.css";
 
-export default function Article() {
-  return <div></div>;
+export default function Article(props) {
+  const { article } = props;
+  return (
+    <article
+      className={`basic, ${article.class}`}
+      style={{ backgroundImage: `url(${article.image})` }}
+    >
+      <img src={article.image} alt="" />
+      <h3>{article.title}</h3>
+    </article>
+  );
 }
--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,5 +1,44 @@
 import React from "react";
+import Article from "./Article";
 
 export default function LandingPage() {
-  return <div>Welcome to our page</div>;
+  const articles = [
+    {
+      title: "Safe travelling in time of Covid-19",
+      image: "../assets/img/covid.png",
+      alt: "Women in mask staying before the plane",
+      class: "covid",
+      text:
+        "Explore  new procedures implemented by airlines because of Covid-19 as well as tips on the most safety travelling in these days.",
+    },
+    {
+      title: "Travel and entry requirements",
+      image: "../assets/img/requirements.png",
+      alt: "row of the planes at airport",
+      class: "requirements",
+      text:
+        "Check the latest information on country/region travel restrictions as well as visa and passport requirements",
+    },
+    {
+      title: "About us",
+      image: "../assets/img/about.png",
+      alt: "Silhouette women throwing paper plane",
+      class: "about",
+      text: "Meet people responsible for this site",
+    },
+    {
+      title: "Terms of service",
+      image: "../assets/img/terms.png",
+      alt: "people waiting at the airport",
+      class: "terms",
+      text: "Learn what are the rules of using this website.",
+    },
+  ];
+  return (
+    <section>
+      {articles.map((article, index) => {
+        return <Article article={article} key={index} />;
+      })}
+    </section>
+  );
 }
