54ce90869db539ed4b13fef4646b35854e87b463
WILLOID <socialguille@gmail.com>
2020-12-28 18:17:29 +0100

    styled cards

--- frontend/src/components/offerCard.jsx
+++ frontend/src/components/offerCard.jsx
@@ -17,8 +17,8 @@ export default function OfferCard(props) {
         <h2>
           <span className="cityName">{offer.departure}</span> to <span className="cityName">{offer.destination}</span>
         </h2>
-        <h4>departure time: {offer.hourOfStart}</h4>
-        <h4>arrival time: {offer.hourOfLanding}</h4>
+        <h4>departure time: {offer.hourOfStart.slice(0,5)}</h4>
+        <h4>arrival time: {offer.hourOfLanding.slice(0,5)}</h4>
         <h3>From {offer.price} €</h3>
         <Button type="submit">Reserve Seat</Button>
       </div>
