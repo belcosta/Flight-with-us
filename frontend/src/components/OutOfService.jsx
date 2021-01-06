a161f9ea233d85912f1bd0d79903a02bcd59b8d8
Alice-Rez <reznickova.alice@seznam.cz>
2021-01-06 16:33:27 +0100

    adding text

--- frontend/src/components/OutOfService.jsx
+++ frontend/src/components/OutOfService.jsx
@@ -6,6 +6,7 @@ export default function OutOfService() {
   return (
     <div className="out-of-service">
       <img src={outOfServiceLogo} alt="" />
+      <p>We are terribly sorry, but our server is currently out of order :(</p>
     </div>
   );
 }
