4f70824128e956ae763007d7bf0120f90547d225
belccarvalho <belccarvalho@gmail.com>
2021-01-15 12:46:54 +0100

    tags substituition and take bootstrap classes out

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import Axios from "axios";
 import appContext from "../context";
-import {
-  Button,
-  Col,
-  FormControl,
-  InputGroup,
-  Row,
-  Spinner,
-  Form,
-} from "react-bootstrap";
 import "../styles/Header.css";
 import logo from "../assets/img/logo.svg";
 
@@ -69,116 +60,90 @@ function Header() {
       {/* <Spinner animation="border" role="status">
                 <span className="sr-only">Loading...</span>
             </Spinner> */}
-      <header className="bgHead p-5 container-fluid">
-        <h1 className="mb-5">
-          <span className="gradient">Fly with us</span>{" "}
-          <img src={logo} alt="" />
+      <header className='bgHead p-5 container-fluid'>
+        <h1 className='mb-5'>
+          <span className='gradient'>Fly with us</span>{" "}
+          <img src={logo} alt='' />
         </h1>
-        <div className="test">
-          <Row className="m-2 p-2 justify-content-md-center">
-            <Col xs md="4">
-              <Form className="mb-3 ">
-                <Form.Group
-                  controlId="exampleForm.SelectCustom"
-                  className="border border-dark"
-                >
-                  <Form.Control
-                    as="select"
-                    custom
-                    name="departure"
-                    onChange={getValue}
-                  >
-                    <option value="">DEPARTURE</option>
-                    {cities.map((city) => (
-                      <option value={city.cityId} key={`dep-${city.cityName}`}>
-                        {city.cityName.trim().substr(0, 1).toUpperCase() +
-                          city.cityName
-                            .trim()
-                            .substr(1, city.cityName.length - 1)}
-                      </option>
-                    ))}
-                  </Form.Control>
-                </Form.Group>
-              </Form>
-            </Col>
-            <Col md="auto">
-              <p className="justify-content-center">&#8826; &#8827;</p>
-            </Col>
-            <Col xs md="4">
-              <Form className="mb-3 ">
-                <Form.Group
-                  controlId="exampleForm.SelectCustom"
-                  className="border border-dark"
-                >
-                  <Form.Control
-                    as="select"
-                    custom
-                    name="destination"
-                    onChange={getValue}
-                  >
-                    <option value="">DESTINATION</option>
-                    {cities.map((city) => {
-                      if (city.cityId !== +search.departure) {
-                        return (
-                          <option
-                            value={city.cityId}
-                            key={`dest-${city.cityName}`}
-                          >
-                            {city.cityName.trim().substr(0, 1).toUpperCase() +
-                              city.cityName
-                                .trim()
-                                .substr(1, city.cityName.length - 1)}
-                          </option>
-                        );
-                      }
-                      return null;
-                    })}
-                  </Form.Control>
-                </Form.Group>
-              </Form>
-            </Col>
-          </Row>
-          <Row className="m-2 p-2 justify-content-md-center">
-            <Col xs sm="2">
-              <InputGroup className="mb-3">
-                <FormControl
-                  type="date"
-                  placeholder="From"
-                  aria-label="from which date"
-                  size="sm"
-                  className="border border-dark"
-                  name="dateFrom"
+        <form>
+          <h4>Book a Flight</h4>
+          <section className='search'>
+            <div className='route'>
+              <div className='box-input'>
+                <select custom name='departure' onChange={getValue}>
+                  <option value=''>DEPARTURE</option>
+                  {cities.map((city) => (
+                    <option value={city.cityId} key={`dep-${city.cityName}`}>
+                      {city.cityName.trim().substr(0, 1).toUpperCase() +
+                        city.cityName
+                          .trim()
+                          .substr(1, city.cityName.length - 1)}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              {/* <p>&#8826; &#8827;</p> */}
+              <div className='box-input'>
+                <select custom name='destination' onChange={getValue}>
+                  <option value=''>DESTINATION</option>
+                  {cities.map((city) => {
+                    if (city.cityId !== +search.departure) {
+                      return (
+                        <option
+                          value={city.cityId}
+                          key={`dest-${city.cityName}`}
+                        >
+                          {city.cityName.trim().substr(0, 1).toUpperCase() +
+                            city.cityName
+                              .trim()
+                              .substr(1, city.cityName.length - 1)}
+                        </option>
+                      );
+                    }
+                    return null;
+                  })}
+                </select>
+              </div>
+            </div>
+
+            <div className='date'>
+              <div className='box-input'>
+                <label htmlFor='dateFrom'>Outbound</label>
+                <input
+                  type='date'
+                  aria-label='from which date'
+                  size='sm'
+                  id='dateFrom'
+                  name='dateFrom'
                   onChange={getDateValue}
                 />
-              </InputGroup>
-            </Col>
-            <Col md="1"></Col>
-            <Col xs sm="2">
-              <InputGroup>
-                <FormControl
-                  type="date"
-                  placeholder="to"
-                  aria-label="until when"
-                  size="sm"
-                  className="border border-dark"
-                  name="dateTo"
+              </div>
+              <div className='box-input'>
+                <label htmlFor='dateTo'>Return Flight</label>
+                <input
+                  type='date'
+                  placeholder='to'
+                  aria-label='until when'
+                  size='sm'
+                  id='dateTo'
+                  name='dateTo'
                   onChange={getDateValue}
                 />
-              </InputGroup>
-            </Col>
-          </Row>
-          <Row className="justify-content-center">
-            <Button
-              type="submit"
-              value="Submit"
-              size="md"
-              className="justify-content-center"
-              onClick={getResults}
-            >
-              Search
-            </Button>
-          </Row>
-        </div>
+              </div>
+            </div>
+            <div className='searchButton'>
+              <button
+                type='submit'
+                value='Submit'
+                size='md'
+                className='btn'
+                onClick={getResults}
+              >
+                Search
+              </button>
+            </div>
+          </section>
+        </form>
       </header>
     </>
   );
